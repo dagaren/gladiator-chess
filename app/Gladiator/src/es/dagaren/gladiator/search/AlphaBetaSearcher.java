c1785838d9fa31ecac30027f31b867365f039e4d
David Garcinuño Enríquez <dagaren@gmail.com>
2011-01-21 00:19:41 +0100

    Se añade el paquete 'es.dagaren.gladiator.search'
    Se añade el paquete 'es.dagaren.gladiator.search' junto con las 
    siguientes clases:
    
     - SearchInfo. Representa la información del resultado de una
     búsqueda
     
     - SearcherObserver. Interfaz con los métodos que debe implementar
     quien necesite ser avisado de los eventos que se produzcan en el
     buscador.
     
     - Searcher. Clase que tiene los métodos y propiedades básicos para
     realizar una búsqueda en una posición.
     
     - AlphaBetaSearcher. Clase que implementa una búsqueda 
     mediante el algoritmo alfa-beta.

---
+++ app/Gladiator/src/es/dagaren/gladiator/search/AlphaBetaSearcher.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright (c) 2008-2011, David Garcinuño Enríquez <dagaren@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.dagaren.gladiator.search;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import es.dagaren.gladiator.notation.Notation;
+import es.dagaren.gladiator.representation.Colour;
+import es.dagaren.gladiator.representation.Movement;
+import es.dagaren.gladiator.representation.Position;
+
+/**
+ * @author dagaren
+ *
+ */
+public class AlphaBetaSearcher extends Searcher
+{
+   protected long numUpdates;
+   
+   protected final int INITIAL_ALFA = -10000 - 1;
+   protected final int INITIAL_BETA =  10000 + 1;
+   
+   @Override
+   public void search()
+   {
+      numUpdates = 0;
+      
+      visitedNodes = 0;
+      cutoffs = 0;
+      
+      int score = 0;
+      
+      //Se implementa la profundidad iterativa
+      for(deep = 2; deep <= deepLimit; deep++)
+      {
+         score = alphaBeta(position, deep, INITIAL_ALFA, INITIAL_BETA, principalVariation);
+      }
+      
+      if(position.getTurn() == Colour.BLACK)
+      {
+         score = -score;
+      }
+      
+      System.err.println("* Num actualizaciones: " + numUpdates);
+      System.err.println("* Nodos recorridos: " + visitedNodes);
+      System.err.println("* Cortes producidos: " + cutoffs);
+      System.err.println("* Puntaje devuelto: " + score);
+   }
+   
+   public int alphaBeta(Position position, int currentDeep, int alpha, int beta, LinkedList<Movement> parentPrincipalVariation)
+   {
+      Colour turn = position.getTurn();
+      
+      LinkedList<Movement> currentPrincipalVariation = new LinkedList<Movement>();
+      
+      //Se acualiza el número de nodos recorridos
+      visitedNodes++;
+      
+      //TODO Se chequean las tablas por repetición
+      
+      //TODO Se busca en las tablas de transposición
+      
+      //TODO Comprobación en la tabla de finales
+      
+      //Se generan los movimientos
+      List<Movement> moves = position.getMovements();
+      
+      if(moves.size() == 0)
+      {
+         //Si no hay ningún movimiento legal en la posición
+         //quiere decir que la posición es tablas por ahogado
+         //o jaque mate dependiendo de si la posición es de jaque
+        
+         parentPrincipalVariation.clear();
+         if(position.isInCheck(turn))
+         {
+            //Es jaque mate
+            return CHECKMATE_SCORE;
+         }
+         else
+         {
+            //Son tablas por ahogado
+            return DRAW_SCORE;
+         }
+      }
+      
+      //Si se ha llegado a la máxima profundidad
+      //Se devuelve una búsqueda quiescence
+      //para evitar el efecto horizonte
+      if(currentDeep == 0)
+      {
+         return alphaBetaQuiescence(position, 8, alpha, beta, parentPrincipalVariation);
+      }
+      
+      //Se itera a través de cada movimiento y se realiza
+      //la búsqueda en las posiciones que resultan
+      for(Movement move: moves)
+      {
+         currentPrincipalVariation.clear();
+         
+         //Se realiza el siguiente movimiento en la lista
+Position clone = position.getCopy();
+String fenBefore = position.toFen();
+         position.doMove(move);
+         
+         //Se calcula la evaluación del movimiento
+         int score = - alphaBeta(position, currentDeep - 1, -beta, -alpha, currentPrincipalVariation);
+      
+         //Se deshace el movimiento
+         position.undoMove(move);
+String fenAfter = position.toFen();
+if(!fenBefore.equals(fenAfter))
+{
+   System.err.println("ERROR FEN ANTES Y DESPUES DE HACER DESHACER NO QUEDA IGUAL: " + Notation.toString(move));
+   System.err.println("Position inicial:");
+   System.err.println("FEN inicial: " + fenBefore);
+   System.err.println(clone.toString());
+   System.err.println("FEN final: " + fenAfter);
+   System.err.println(position.toString());
+}
+         
+         if(score >= beta)
+         {
+            //Si la puntuación supera beta
+            //se produce un corte
+            cutoffs++;
+            
+            parentPrincipalVariation.clear();
+            
+            return beta;
+         }
+         if(score > alpha)
+         {
+            //si la puntuación devuelta supera alfa
+            //se ha encontrado un movimiento mejor
+            alpha = score;
+            
+            numUpdates++;
+            
+            parentPrincipalVariation.clear();
+            parentPrincipalVariation.add(move);
+            parentPrincipalVariation.addAll(currentPrincipalVariation);
+            
+            if(currentDeep == deep)
+            {
+               if(turn == Colour.WHITE)
+                  bestScore = score;
+               else
+                  bestScore = -score;
+               
+               publishInfo();
+            }
+         }
+      }
+      
+      return alpha;
+   }
+   
+   public int alphaBetaQuiescence(Position position, int currentDeep, int alpha, int beta, LinkedList<Movement> parentPrincipalVariation)
+   {
+      Colour turn = position.getTurn();
+      
+      LinkedList<Movement> currentPrincipalVariation = new LinkedList<Movement>();
+      
+      //Se actualiza el número de posiciones recorridas
+      visitedNodes++;
+      
+      //Se generan los movimientos
+      List<Movement> moves = position.getMovements();
+      List<Movement> captureMoves = position.getCaptureMovements();
+      
+      if(moves.size() == 0)
+      {
+         //Si no hay ningún movimiento legal en la posición
+         //quiere decir que la posición es tablas por ahogado
+         //o jaque mate dependiendo de si la posición es de jaque
+        
+         parentPrincipalVariation.clear();
+         if(position.isInCheck(turn))
+         {
+            //Es jaque mate
+            return CHECKMATE_SCORE;
+         }
+         else
+         {
+            //Son tablas por ahogado
+            return DRAW_SCORE;
+         }
+      }
+      
+      //Se calcula la evaluación estática de la posicion
+      int score = evaluator.evaluate(position);
+      
+      if(captureMoves.size() == 0)
+      {
+         parentPrincipalVariation.clear();
+         return score;
+      }
+      
+      //Se intenta actualizar alfa con el puntaje devuelto
+      //Esto es necesario ya que como solo se van a comprobar 
+      //los movimientos de captura es necesario establecer 
+      //este valor para que se vea si las capturas generan
+      //mejor resultado, y si no devolver el valor de la puntuación 
+      //estática.
+      if(score >= beta)
+      {
+         parentPrincipalVariation.clear();
+         
+         return beta;
+      }
+      if(alpha < score)
+      {
+         alpha = score;
+      }
+      
+      //Se itera a través de cada movimiento de captura y se realiza
+      //la búsqueda en las posiciones que resultan
+      for(Movement move: captureMoves)
+      {
+         currentPrincipalVariation.clear(); 
+         
+         //Se realiza el siguiente movimiento en la lista
+Position clone = position.getCopy();
+String fenBefore = position.toFen();
+         position.doMove(move);
+      
+         //Se calcula la puntuación del movimiento
+         score = - alphaBetaQuiescence(position, currentDeep - 1, -beta, -alpha, currentPrincipalVariation);
+         
+         //Tras evaluar el movimiento se deshace
+         position.undoMove(move);
+String fenAfter = position.toFen();
+if(!fenBefore.equals(fenAfter))
+{
+   System.err.println("ERROR FEN ANTES Y DESPUES DE HACER DESHACER NO QUEDA IGUAL: (quiescence) "  + Notation.toString(move));
+   System.err.println("Position inicial:");
+   System.err.println("FEN inicial: " + fenBefore);
+   System.err.println(clone.toString());
+   System.err.println("FEN final: " + fenAfter);
+   System.err.println(position.toString());
+   System.exit(0);
+}
+         
+         if(score >= beta)
+         {
+            cutoffs++;
+            
+            parentPrincipalVariation.clear();
+            
+            return beta;
+         }
+         if(score > alpha)
+         {
+            numUpdates++;
+            
+            parentPrincipalVariation.clear();
+            parentPrincipalVariation.add(move);
+            parentPrincipalVariation.addAll(currentPrincipalVariation);
+            
+            alpha = score;
+         }
+      }
+      
+      return alpha;
+      
+   }
+}
---
+++ app/Gladiator/src/es/dagaren/gladiator/search/Searcher.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright (c) 2008-2011, David Garcinuño Enríquez <dagaren@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package es.dagaren.gladiator.search;
+
+import java.util.LinkedList;
+
+import es.dagaren.gladiator.evaluation.Evaluator;
+import es.dagaren.gladiator.representation.Movement;
+import es.dagaren.gladiator.representation.Position;
+
+/**
+ * @author dagaren
+ *
+ */
+public class Searcher implements Runnable
+{
+   protected int deepLimit;
+   protected long timeLimit;
+   protected long nodesLimit;
+   
+   protected long initTime = 0;
+   
+   protected long visitedNodes;
+   protected long cutoffs;
+   
+   protected volatile boolean search;
+   protected volatile boolean exit;
+   
+   protected int bestScore;
+   protected int deep;
+   
+   protected LinkedList<Movement> principalVariation;
+   
+   protected Thread thread;
+   
+   protected Position position;
+   
+   protected Evaluator evaluator;
+   
+   protected final int DRAW_SCORE = 0;
+   protected final int CHECKMATE_SCORE = -10000;
+   
+   protected final int MIN_RATING =  -10000 - 1;
+   protected final int MAX_RATING =   10000 + 1;
+   
+   protected SearcherObserver observer;
+   
+   public Searcher()
+   {
+      deepLimit = 4;
+      timeLimit = -1;
+      nodesLimit = -1;
+      
+      principalVariation = new LinkedList<Movement>();
+      
+      evaluator = new Evaluator();
+      
+      exit = false;
+      search = false;
+      
+      thread = new Thread(this);
+      thread.start();
+   }
+   
+   public void run()
+   {
+      searchLoop();
+   }
+   
+   public synchronized void initSearch(Position position)
+   {
+      this.position = position.getCopy();
+      
+      search = true;
+      
+      notifyAll();
+   }
+   
+   public synchronized void finish()
+   {
+      exit = true;
+      search = false;
+      notifyAll();
+   }
+   
+   public synchronized void searchLoop()
+   {
+      while(!exit)
+      {
+         synchronized(this)
+         {
+            while(!search && !exit)
+            {
+               try
+               {
+                  wait();
+               }
+               catch(Exception ex)
+               {}
+            }
+            
+            if(exit == true)
+               break;
+            
+            //Se limpia la variante principal
+            principalVariation = new LinkedList<Movement>();
+            
+            //Se resetea los nodos recorridos
+            visitedNodes = 0;
+            
+            //Se inicia el tiempo de búsqueda
+            initTime = System.currentTimeMillis();
+           
+            //Se llama a la función de búsquda
+            search();
+            
+            search = false;
+            
+            if(observer != null)
+               observer.onSearchFinished(this);
+         }
+      }
+   }
+   
+   protected void publishInfo()
+   {
+      long time = System.currentTimeMillis();
+      
+      SearchInfo info = new SearchInfo();
+      info.time = time - initTime;
+      info.principalVariation = this.principalVariation;
+      info.nodes = this.visitedNodes;
+      info.deep = this.deep;
+      info.score = this.bestScore;
+      
+      if(observer != null)
+         observer.onInformationPublished(info);
+   }
+   
+   public void search()
+   {
+      
+   }
+   
+   public synchronized void stop()
+   {
+      search = false;
+   }
+
+   public int getDeepLimit()
+   {
+      return deepLimit;
+   }
+
+   public void setDeepLimit(int deepLimit)
+   {
+      this.deepLimit = deepLimit;
+   }
+
+   public long getTimeLimit()
+   {
+      return timeLimit;
+   }
+
+   public void setTimeLimit(long timeLimit)
+   {
+      this.timeLimit = timeLimit;
+   }
+
+   public long getNodesLimit()
+   {
+      return nodesLimit;
+   }
+
+   public void setNodesLimit(long nodesLimit)
+   {
+      this.nodesLimit = nodesLimit;
+   }
+
+   public long getCutoffs()
+   {
+      return cutoffs;
+   }
+
+   public void setCutoffs(long cutoffs)
+   {
+      this.cutoffs = cutoffs;
+   }
+
+   public int getBestScore()
+   {
+      return bestScore;
+   }
+
+   public void setBestScore(int bestScore)
+   {
+      this.bestScore = bestScore;
+   }
+
+   public int getDeep()
+   {
+      return deep;
+   }
+
+   public void setDeep(int deep)
+   {
+      this.deep = deep;
+   }
+
+   public LinkedList<Movement> getPrincipalVariation()
+   {
+      return principalVariation;
+   }
+
+   public void setPrincipalVariation(LinkedList<Movement> principalVariation)
+   {
+      this.principalVariation = principalVariation;
+   }
+
+   public Position getPosition()
+   {
+      return position;
+   }
+
+   public void setPosition(Position position)
+   {
+      this.position = position;
+   }
+
+   public Evaluator getEvaluator()
+   {
+      return evaluator;
+   }
+
+   public void setEvaluator(Evaluator evaluator)
+   {
+      this.evaluator = evaluator;
+   }
+
+   public SearcherObserver getObserver()
+   {
+      return observer;
+   }
+
+   public void setObserver(SearcherObserver observer)
+   {
+      this.observer = observer;
+   }
+
+   public long getVisitedNodes()
+   {
+      return visitedNodes;
+   }
+   
+   
+}
