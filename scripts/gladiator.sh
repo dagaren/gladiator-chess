4acbdfc0ebb0868383c4caea2627a29d3ba52243
David Garcinuño Enríquez <dagaren@gmail.com>
2012-08-02 01:34:01 +0200

    Se añade plugin a pom para copiar los ficheros que se generan en la construcción del proyecto a la carpeta releases

--- scripts/gladiator.sh
+++ scripts/gladiator.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+RELEASE=$1
+
 SCRIPTS_DIR=$(dirname $0)
-GLADIATOR_JAR=$SCRIPTS_DIR/../app/Gladiator/bin/Gladiator.jar
+GLADIATOR_JAR=$SCRIPTS_DIR/../releases/$RELEASE
 
 java -Xmx2048M -jar "$GLADIATOR_JAR" 
